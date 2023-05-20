6635b25378ed1fb5057524eb6404365e2ea38319
GuluzadehSenan <guluzadehsenan@gmail.com>
2023-05-20 11:40:38 +0400

    varuable.py fayli gonderildi

---
+++ varuable.py
@@ -0,0 +1,66 @@
+a = 5
+b = 33
+C = b / 3
+C -= 1
+D = b // (a + C)
+A1 = ((a+b) * D) % C
+print = A1
+print = type(A1)
+
+
+q = 12
+w = 180
+e = 17
+t = 19
+y = 30
+w -= 80
+R = t / w
+t -= 9
+U = y / t
+Besiktas = R * w * w * t + U
+print = Besiktas
+print = type(Besiktas)
+print = type(R)
+
+
+Z = 18
+X = 48
+C = Z - X
+V = C / 3
+N = V // 4
+M = N % V % 3
+print = M
+print = type(M)
+
+
+S = 20292
+G = 37389
+H = 48940
+S /= 2
+S /= 2
+G /= 3
+G /= 11
+H /= 5
+H /= 4
+P = S * G // H
+P -= 1973
+k = P / 12
+print = k
+print = type(k)
+
+
+su = 20
+qum = 100
+toy = 7
+su *= 5
+qum //= 10
+su -= 50
+qum += 40
+A = su * qum
+D = su + qum * 5
+qum -= 10
+J = (su - qum) * 2
+L = (su - qum) % toy
+bu_il = A - D + J + L
+print = +bu_il
+print = type(bu_il)
