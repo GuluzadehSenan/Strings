ca137403e3f7eb9e987568508614a919d87561a9
GuluzadehSenan <guluzadehsenan@gmail.com>
2023-05-20 11:16:40 +0400

    strings.py faylini gonderdim

---
+++ strings.py
@@ -0,0 +1,57 @@
+mark = "perakende"
+# perde
+print(mark[0:3] + mark[7:9])
+# kenarda
+print(mark[4:7] + mark[3:1:-1] + mark[7:2:-4])
+# 
+
+#  menim adim quluzade Senandir.menim 18 yasim var.men unec -de (azerbaycan dovlet Iqtisad universitetinde)
+# telebeyem. iqtisadiyyat ve Idareetme fakultesinde OXUYURAM. men unec-in genclik korpusunda tehsil aliram.
+
+
+a = "menim adim"
+a = a.capitalize()
+b = "quluzade"
+b = b.capitalize()
+b1 = "Senandir."
+
+c = "menim"
+c = c.capitalize()
+c1 = 18
+d = "YASIM VAR."
+d = d.lower()
+
+e = "men"
+e = e.capitalize()
+f = "unec"
+f = f.upper()
+g = "-de"
+h = "(Azerbaycan dovlet Iqtisad universitetinde)"
+j = "telebeyem."
+
+k = "iqtisadiyyat"
+k = k.capitalize()
+l = "ve Idareetme fakultesinde"
+z = "OXUYURAM."
+z = z.lower()
+
+c = "men"
+c = c.capitalize()
+x = "unec"
+x = x.upper()
+q = "-in"
+w = "genclik korpusunda tehsil aliram."
+w = w.capitalize()
+
+
+print(a, b, b1)
+
+print(c, c1, d)
+
+print(e, f + g, h, j)
+
+print(k, l, z)
+
+print(c, x + q, w)
+
+print(a, b, b1, c, c1, d, e, f + g, h, j, k, l, z, c, x + q, w)
