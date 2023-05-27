f5c35c917c4068f20fd273dec29b49c79af029af
GuluzadehSenan <guluzadehsenan@gmail.com>
2023-05-27 05:34:39 +0400

    story_1 fayli gonderildi

---
+++ story_1.py
@@ -0,0 +1,50 @@
+# Fizik,riyaziyyatci,kimyager,geoloq,antropoloqdan ibaret heyet açiq havada tedqiqat üçün bir araya gelmişdirler.
+
+# a1 = "ibaret heyet açiq havada tedqiqat üçün bir araya gelmişdirler."
+# a1=print("Salamlar.Hekayemiz baslayir." "\n\tFizik","Riyaziyyatci", "Kimayager","Geoloq", "Antropoloqdan",sep=",")
+# a2="ibaret heyet açiq havada tedqiqat üçün bir araya gelmişdirler."
+# Onlar işlərini bitirdikdən sonra dincəlmək üçün yaxınlıqdakı evə yığışdılar.
+a3 = "Bu arada tedqiqatcilarin diqqetini yerden bir metr yuxarida,altinda xirda daslar olan soba celb etdi."
+s = "Fizik: Ev sahibi sobani yuxarida ona gore qoyub ki,konveksiya yolu ile otaq qisa müddetde daha cox isinsin."
+d = "Kimyager:Soba yüksekde qurularaq aktiv enerjinin asagi düsmesine komek eder ve belelikle isinme daha da tezlesmis olar."
+c = "Geoloq: Bura tektonik zona oldugundan zelzele bas vermesi neticesinde sobanin altdaki daslarin uzerine dusmesi evdeki yangin riskini azalda biler."
+w = "Riyaziyyatci: Soba otagin tam merkezinde oldugundan isinme daha duzgun yayilir."
+f = "Antropoloq: Kecmisde olan inanca gore od muqeddes sayildigindan ona hormet olaraq soba daha yuxarida qoyulub."
+p = "Boru catmadi ona gore!"
+
+print(
+    "Salamlar.Hekayemiz baslayir." "\n\tFizik",
+    "Riyaziyyatci",
+    "Kimayager",
+    "Geoloq",
+    "Antropoloqdan",
+    sep=",",
+    end="," "ibaret heyet aciq havada tedqiqat ücün bir araya gelmisdirler.",
+)
+
+print("Onlar islerini bitirdikden sonra dincelmek üçün yaxinliqdaki eve yigisdilar.")
+
+print("Ev sahibi onlara qulluq etmek ücün evden ayrildi.")
+
+print(a3)
+
+print(
+    "Sobanin niye bele qoyulduguna müzakireler baslayir:",
+    "\n",
+    s,
+    "\n",
+    d,
+    "\n",
+    c,
+    "\n",
+    w,
+    "\n",
+    f,
+)
+
+print(
+    "Bu sirada ev sahibi iceri girer.Ve ondan bunun sebebi sorusulduqda O,bele cavab verer:",
+    "\n",
+    p,
+)
+
